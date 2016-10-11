acd0b32c302bb04af5f91495b87ec351f80d8772
Harshil Darji <darjiharshil2994@gmail.com>
2016-10-11 23:25:25 +0530

    basic changes

--- Database/login/index.php
+++ Database/login/index.php
@@ -1,11 +1,25 @@
 <?php
 session_start();
-if(isset($_SESSION['uid']) && !empty($_SESSION['uid'])){
-    require('connect.php');
-    $query = "SELECT `name` FROM `users` WHERE `id` = '".$_SESSION['uid']."'";
-    $uname = mysql_result(mysql_query($query), 0, 'name');
-    echo "Wel Come <strong>".ucfirst($uname)."</strong>!<br><a href = 'logout.php'>Log out</a>";
-} else{
-    include('login.php');
+
+function loggedin(){
+    if(isset($_SESSION['uid']) && !empty($_SESSION['uid'])){
+        user_data($_SESSION['uid']);
+    } else{
+        include('login.php');
+    }
 }
+
+function user_data($uid){
+    require('connect.php');
+    $id = $uid;
+    $query = "SELECT * FROM `users` WHERE `id` = '".$id."'";
+    $rows = mysql_fetch_assoc(mysql_query($query));
+    $name = $rows['name'];
+    $dist = $rows['district'];
+    
+    echo 'Wel come <strong>'.ucfirst($name).'</strong> [from: <strong>'.ucfirst($dist).'</strong>]<br>';
+    echo "<a href = 'logout.php'>Log out</a>";
+}
+
+loggedin();
 ?>
